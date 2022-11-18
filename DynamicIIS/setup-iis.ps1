36f5da4ee10acf1854de2ed4d809312d8ba438e3
Gustavo Kuno <gustavo.mkuno@outlook.com>
2022-11-17 22:27:35 -0300

    .

--- DynamicIIS/setup-iis.ps1
+++ DynamicIIS/setup-iis.ps1
@@ -25,11 +25,56 @@ param (
   [string] $envName = "aceitacao",
   [string] $Domain = "microvix.com.br",
   [bool] $UseCustomUsername = $false,
-  [bool] $isDev = $false
+  [bool] $isDev = $false,
+  [bool] $PurgeDefaults = $true,
+  [bool] $setWebServerDefaults = $true 
 
 )
+import-module WebAdministration
+$AppCmd = "$env:WinDir\system32\inetsrv\AppCmd.exe"
 
-# TODO: Cria site base e webapps apenas EM DEV
+# Remove Defaults
+if ($PurgeDefaults) {
+  
+  if ((Test-Path "IIS:\sites\Default Web Site") -eq $true) {
+    # Delete site "Default Web Site"
+    & $AppCmd delete site "Default Web Site"
+  }
+  $AppPoolDft = @('Classic .NET AppPool', '.NET v2.0 Classic', '.NET v2.0', '.NET v4.5 Classic', '.NET v4.5', 'DefaultAppPool')
+  foreach ($a in $AppPoolDft) {
+    if ((Test-Path "IIS:\AppPools\$a") -eq $true) {
+      # Application pool doesn't exist, create it...
+      & $AppCmd  delete AppPool $a
+    }
+  }
+}
+
+if ($setWebServerDefaults) {
+  # Server Config
+  & $AppCmd unlock config /section:system.webServer/handlers
+  & $AppCmd unlock config /section:system.webServer/modules
+  & $AppCmd unlock config /section:system.webServer/asp
+}
+
+
+$microvixSiteLists = @(
+  [pscustomobject]@{Name = "crm-app" ; }
+  [pscustomobject]@{Name = "crm-api" ; }
+  [pscustomobject]@{Name = "vendafacil" ; }
+  [pscustomobject]@{Name = "estoque" ; }
+  [pscustomobject]@{Name = "wms" ; }
+  [pscustomobject]@{Name = "hubvaletrocas" ; }
+  [pscustomobject]@{Name = "agendaservicos-app" ; }
+  [pscustomobject]@{Name = "implantar" ; }
+  [pscustomobject]@{Name = "erp-app" ; }
+  [pscustomobject]@{Name = "recuperadorcupomfiscal" ; }
+  [pscustomobject]@{Name = "" ; }
+  [pscustomobject]@{Name = "" ; }
+  [pscustomobject]@{Name = "" ; }
+
+) 
+
+
 if ($isDev) {
   $siteList = @(
     [pscustomobject]@{Name = "${hostname}" ; Bindings = "${hostname}-.${Domain}" }
@@ -38,34 +83,25 @@ if ($isDev) {
     [pscustomobject]@{Name = "wms" ; Bindings = "${hostname}-wms-${envName}.${Domain}" }
   ) 
 } 
+
 else {
   $siteList = @(
-    [pscustomobject]@{Name = "crm" ; Bindings = "crm-${hostname}.${Domain}" }
-    [pscustomobject]@{Name = "vendafacil" ; Bindings = "vendafacil-${hostname}-${envName}.${Domain}" }
-    [pscustomobject]@{Name = "estoque" ; Bindings = "estoque${hostname}-${envName}.${Domain}" }
-    [pscustomobject]@{Name = "wms" ; Bindings = "wms${hostname}-${envName}.${Domain}" }
+    [pscustomobject]@{Name = "crm" ; Bindings = "$($sitelist[0].name)-${envName}.${Domain}" }
+    [pscustomobject]@{Name = "vendafacil" ; Bindings = "$($sitelist[1].name)-${envName}.${Domain}" }
+    [pscustomobject]@{Name = "estoque" ; Bindings = "$($sitelist[2].name)-${envName}.${Domain}" }
+    [pscustomobject]@{Name = "wms" ; Bindings = "$($sitelist[3].name)-${envName}.${Domain}" }
+    [pscustomobject]@{Name = "hubvaletrocas" ; Bindings = "$($sitelist[4].name)-${envName}.${Domain}" }
+    [pscustomobject]@{Name = "agendaservicos" ; Bindings = "$($sitelist[5].name)-${envName}.${Domain}" }
+    [pscustomobject]@{Name = "implantar" ; Bindings = "$($sitelist[6].name)-${envName}.${Domain}" }
+
   ) 
 }
-
-import-module WebAdministration
-$AppCmd = "$env:WinDir\system32\inetsrv\AppCmd.exe"
 
 # defining Default Values 
 $sitePath = "${pathWebSite}\${envName}"
 write-Output "site path: ${sitePath}"
 
-# Remove Defaults
-if ((Test-Path "IIS:\sites\Default Web Site") -eq $true) {
-  # Delete site "Default Web Site"
-  & $AppCmd delete site "Default Web Site"
-}
-$AppPoolDft = @('Classic .NET AppPool', '.NET v2.0 Classic', '.NET v2.0', '.NET v4.5 Classic', '.NET v4.5', 'DefaultAppPool')
-foreach ($a in $AppPoolDft) {
-  if ((Test-Path "IIS:\AppPools\$a") -eq $true) {
-    # Application pool doesn't exist, create it...
-    & $AppCmd  delete AppPool $a
-  }
-}
+
 
 Foreach ($item in $siteList) {
   [string] $projectName = ($item).Name
@@ -95,49 +131,40 @@ Foreach ($item in $siteList) {
 
   ##################### END BASIC #################################
 
-  # Server Config
-  & $AppCmd unlock config /section:system.webServer/handlers
-  & $AppCmd unlock config /section:system.webServer/modules
-  & $AppCmd unlock config /section:system.webServer/asp
-
-
-}
 
 
 
+  break 
 
 
-break 
-
-
-# Não usar o código abaixo
+  # Não usar o código abaixo
 
 
 
-$webSite = "teste3"
-$appPool = "testeAppPool2"
-$dnsFqdn = "website.local.net"
-$pathWebSite = "c:\temp\"
-$pathLogs = "c:\site\logs"
+  $webSite = "teste3"
+  $appPool = "testeAppPool2"
+  $dnsFqdn = "website.local.net"
+  $pathWebSite = "c:\temp\"
+  $pathLogs = "c:\site\logs"
 
-$identity = @{ identitytype = "SpecificUser"; username = "My Username"; password = "My Password" }
-$settings = @{ logFormat = "W3c"; enabled = $true; directory = $pathLogs; period = "Daily"; }
+  $identity = @{ identitytype = "SpecificUser"; username = "My Username"; password = "My Password" }
+  $settings = @{ logFormat = "W3c"; enabled = $true; directory = $pathLogs; period = "Daily"; }
 
-if ((Test-Path "IIS:\AppPools\$appPool") -eq $False) {
-  # Application pool does not exist, create it...
-  New-Item -Path "IIS:\AppPools" -Name $appPool -Type AppPool
-}
-Set-ItemProperty -Path "IIS:\AppPools\$appPool" -name "processModel" -value $identity
-Set-ItemProperty -Path "IIS:\AppPools\My Pool" -name "enable32BitAppOnWin64" -value $true
+  if ((Test-Path "IIS:\AppPools\$appPool") -eq $False) {
+    # Application pool does not exist, create it...
+    New-Item -Path "IIS:\AppPools" -Name $appPool -Type AppPool
+  }
+  Set-ItemProperty -Path "IIS:\AppPools\$appPool" -name "processModel" -value $identity
+  Set-ItemProperty -Path "IIS:\AppPools\My Pool" -name "enable32BitAppOnWin64" -value $true
 
-if ((Test-Path "IIS:\Sites\$webSite") -eq $False) {
-  # Site does not exist, create it...
-  New-WebSite -Name $webSite -Port 80 -HostHeader $dnsFqdn -PhysicalPath $pathWebSite
-}
+  if ((Test-Path "IIS:\Sites\$webSite") -eq $False) {
+    # Site does not exist, create it...
+    New-WebSite -Name $webSite -Port 80 -HostHeader $dnsFqdn -PhysicalPath $pathWebSite
+  }
 
-Set-ItemProperty "IIS:\Sites\$webSite" -name "logFile" -value $settings
+  Set-ItemProperty "IIS:\Sites\$webSite" -name "logFile" -value $settings
 
-if ((Test-Path "IIS:\Sites\$webSite\MyApp") -eq $False) {
-  # App/virtual directory does not exist, create it...
-  # ...
-}
+  if ((Test-Path "IIS:\Sites\$webSite\MyApp") -eq $False) {
+    # App/virtual directory does not exist, create it...
+    # ...
+  }
