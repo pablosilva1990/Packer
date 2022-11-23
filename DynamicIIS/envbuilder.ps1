8e5a14729ead41b9493d4c78ce84c46862f399a0
Gustavo Kuno <gustavo.kuno@linx.com.br>
2022-11-23 11:23:55 -0300

    update script

--- DynamicIIS/envbuilder.ps1
+++ DynamicIIS/envbuilder.ps1
@@ -69,17 +69,8 @@ function start-EnvironmentBuilderApps {
     [string]$ManagedPipelineMode = "Integrated", # Possible Values: "Integrated" or "Classic"
     [string]$startup = "OnDemand",
 
-    # Config - Site
-    [bool]$setWebServerDefaults = $true,
-    [bool]$customTestLogPathEnabled = $false,
-    [string]$logsPath,
-    [bool]$ASPClassicConfig = $true,
-
-    #Security
-    [bool]$configRequestFiltering = $false,
 
     # Purge Parametsr
-    [bool]$PurgeDefaults = $true,
     [bool]$PurgeSites = $false,
     [bool]$PurgeSiteFolder = $false
   )
@@ -107,22 +98,6 @@ function start-EnvironmentBuilderApps {
     if ((Test-Path "IIS:\AppPools\$siteName") -eq $true) {
       # Delete site "Default Web Site"
       & $AppCmd  delete AppPool "${siteName}" | Out-Null
-    }
-  }
-
-  # Remove Defaults
-  if ($PurgeDefaults) {
-  
-    if ((Test-Path "IIS:\sites\Default Web Site") -eq $true) {
-      # Delete site "Default Web Site"
-      & $AppCmd delete site "Default Web Site" | Out-Null
-    }
-    $AppPoolDft = @('Classic .NET AppPool', '.NET v2.0 Classic', '.NET v2.0', '.NET v4.5 Classic', '.NET v4.5', 'DefaultAppPool')
-    foreach ($a in $AppPoolDft) {
-      if ((Test-Path "IIS:\AppPools\$a") -eq $true) {
-        # Application pool doesn't exist, create it...
-        & $AppCmd  delete AppPool $a | Out-Null
-      }
     }
   }
 
@@ -195,109 +170,9 @@ function start-EnvironmentBuilderApps {
     & $AppCmd set AppPool $siteName /recycling.periodicRestart.privateMemory:'10485760' /commit:apphost | Out-Null
   }
   
-  ## Server Defaults
-  if ($setWebServerDefaults) {
-    # Server Config
-    & $AppCmd unlock config /section:system.webServer/handlers | Out-Null
-    & $AppCmd unlock config /section:system.webServer/modules | Out-Null
-    & $AppCmd unlock config /section:system.webServer/asp | Out-Null
 
-    if ($customTestLogPathEnabled) {
-      $testLogPath = test-path -path $logsPath
-      If ($testLogPath -eq $false) {
-        new-item -type Directory -path $logsPath | Out-Null
-      }
-      ## SET LOGS CONFIG
-      $LogValues = "Date, Time, ClientIP, UserName, SiteName, ComputerName, ServerIP, Method, UriStem, UriQuery, HttpStatus, Win32Status, BytesSent, BytesRecv, TimeTaken, ServerPort, UserAgent, Cookie, Referer, ProtocolVersion, Host, HttpSubStatus" 
-      $settings = @{ logFormat = "W3c"; enabled = $true; directory = $logsPath; period = "Hourly" ; localTimeRollover = "True" ; logExtFileFlags = "${logValues}" }
-      Set-ItemProperty "IIS:\Sites\$siteName" -name "logFile" -value $settings | Out-Null
-  
-      ## Custom Headers
-      #$logValuesCustom = @{logFieldName = 'X-Forwarded-For'; sourceName = 'X-Forwarded-For'; sourceType = 'RequestHeader' }
-      #Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/site[@name="${siteName}"]/logFile/customFields" -name "." -value $logValuesCustom
 
-    }
-    else {
-      ## SET LOGS CONFIG
-      $LogValues = "Date, Time, ClientIP, UserName, SiteName, ComputerName, ServerIP, Method, UriStem, UriQuery, HttpStatus, Win32Status, BytesSent, BytesRecv, TimeTaken, ServerPort, UserAgent, Cookie, Referer, ProtocolVersion, Host, HttpSubStatus" 
-      $settings = @{ logFormat = "W3c"; enabled = $true; period = "Hourly" ; localTimeRollover = "True" ; logExtFileFlags = "${logValues}" }
-      Set-ItemProperty "IIS:\Sites\$siteName" -name "logFile" -value $settings | Out-Null
-  
-      ## Custom Headers
-      #$logValuesCustom = @{logFieldName = 'X-Forwarded-For'; sourceName = 'X-Forwarded-For'; sourceType = 'RequestHeader' }
-      #Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST'  -filter "system.applicationHost/sites/site[@name="${siteName}"]/logFile/customFields" -name "." -value $logValuesCustom
-    }
-
-    # ASP Classic Section "long live the King"
-
-    & $AppCmd set config -section:system.webServer/asp /scriptErrorSentToBrowser:'True' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /errorsToNTLog:'True' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /EnableParentPaths:'True' /commit:apphost | Out-Null
-
-    # TrackingID MS 2208240040004733 - MTA must be disabled. 
-    # Alterar isso potencializará problemas de performance em magnitudes inimaginaveis
-    & $AppCmd  set config -section:system.webServer/asp /comPlus.executeInMta:'False' /commit:apphost | Out-Null
-
-    & $AppCmd set config -section:system.webServer/asp /Session.timeOut:'00:20:00' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /Limits.bufferingLimit:'10094304' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /Limits.maxRequestEntityAllowed:'2147483647' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /Limits.processorThreadMax:'400' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /Limits.scriptTimeout:'00:10:00' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /cache.scriptFileCacheSize:'0' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /cache.maxDiskTemplateCacheFiles:'0' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /cache.scriptEngineCacheMax:'0' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /cache.enableTypelibCache:'True' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/asp /cache.diskTemplateCacheDirectory:'c:\inetpub\temp\ASP Compiled Templates' /commit:apphost | Out-Null
-
-    & $AppCmd set config -section:system.webServer/urlCompression /doDynamicCompression:'false' /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/httpCompression /minFileSizeForComp:2700 /commit:apphost | Out-Null
-    & $AppCmd set config -section:system.webServer/httpCompression /maxDiskSpaceUsage:16000 /commit:apphost | Out-Null
-  
-
-  }
-
-  if ($configRequestFiltering) {
-    # SYSUSERS
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sysusers', scanUrl='True', scanQueryString='True', scanAllRaw='False']" /commit:apphost | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sysusers'].denyStrings.[string='sysusers']" /commit:apphost | out-null
-
-    # SQLMAP
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sqlmap', scanUrl='True', scanQueryString='True', scanAllRaw='False']" | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sqlmap'].scanHeaders.[requestHeader='User-agent']" | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sqlmap'].denyStrings.[string='sqlmap']" | out-null
-
-    # internet-crawlers
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='internet-crawlers', scanUrl='False', scanQueryString='False', scanAllRaw='False']" | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='internet-crawlers'].scanHeaders.[requestHeader='User-Agent']" | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='internet-crawlers'].denyStrings.[string='python']" | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='internet-crawlers'].denyStrings.[string='got']" | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='internet-crawlers'].denyStrings.[string='rest-client']" | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='internet-crawlers'].denyStrings.[string='mechanize']" | out-null
-
-    # sysdatabases
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sysdatabases', scanUrl='True', scanQueryString='True', scanAllRaw='False']" /commit:apphost | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sysdatabases'].denyStrings.[string='sysdatabases']" /commit:apphost | out-null
-
-    # information_schema
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='information_schema', scanUrl='True', scanQueryString='True', scanAllRaw='False']" /commit:apphost | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='information_schema'].denyStrings.[string='information_schema']" /commit:apphost | out-null
-
-    # sysobjects
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sysobjects', scanUrl='True', scanQueryString='True', scanAllRaw='False']" /commit:apphost | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='sysobjects'].denyStrings.[string='sysobjects']" /commit:apphost | out-null
-
-    # table_schema
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='table_schema', scanUrl='True', scanQueryString='True', scanAllRaw='False']" /commit:apphost | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='table_schema'].denyStrings.[string='table_schema']" /commit:apphost | out-null
-
-    # db_name
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='db_name', scanUrl='True', scanQueryString='True', scanAllRaw='False']" /commit:apphost | out-null
-    & $AppCmd  set config -section:system.webServer/security/requestFiltering /+"filteringRules.[name='db_name'].denyStrings.[string='DB_NAME%28']" /commit:apphost | out-null
-
-    # RequestLimit
-    & $AppCmd set config -section:system.webServer/security/requestFiltering /requestLimits.maxAllowedContentLength:'524288000' /commit:apphost | out-null 
-  }
-
+ 
 
 }
 
@@ -476,7 +351,7 @@ if ($EnvBuilderCleanUp) {
   [string] $projectName = ($item).Name
   $siteBinding = "${projectName}-${envName}.${Domain}"
   $path = "${pathWebSite}\${siteBinding}"
-  remove-WebEnvironmentBuilder -sitePath "${path}" -siteName "${siteBinding}"
+  remove-EnvironmentBuilderApps -sitePath "${path}" -siteName "${siteBinding}"
 }
 
 
