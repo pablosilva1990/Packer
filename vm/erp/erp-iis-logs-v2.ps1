388bb32d6d2ce992785f8678d14791f3242d354c
vinicius.sfernandes <vinicius.sfernandes@linx.com.br>
2023-03-20 11:54:45 -0300

    update custom field - logs vm erp

--- vm/erp/erp-iis-logs-v2.ps1
+++ vm/erp/erp-iis-logs-v2.ps1
@@ -11,6 +11,6 @@ $AppCmd = "$env:WinDir\system32\inetsrv\AppCmd.exe"
 & $AppCmd set config -section:system.applicationHost/sites /siteDefaults.logFile.logTargetW3C:'File' /commit:apphost 
 & $AppCmd set config -section:system.applicationHost/log /centralW3CLogFile.localTimeRollover:"True" /commit:apphost
 & $AppCmd set config -section:system.applicationHost/sites /siteDefaults.logFile.LocalTimeRollover:'True' /commit:apphost 
-& $AppCmd set config -section:system.applicationHost/sites /+"siteDefaults.loFile.customFields.[logFieldName='X-Forwarded-For',sourceName='X-Forwarded-For',sourceType='RequestHeader']" /commit:apphost
+& $AppCmd set config -section:system.applicationHost/sites /+"siteDefaults.logFile.customFields.[logFieldName='X-Forwarded-For',sourceName='X-Forwarded-For',sourceType='RequestHeader']" /commit:apphost
 & $AppCmd set config -section:system.applicationHost/sites /siteDefaults.logFile.logExtFileFlags:"Date,Time,ClientIP,UserName,SiteName,ComputerName,ServerIP,Method,UriStem,UriQuery,HttpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,Cookie,Referer,ProtocolVersion,Host,HttpSubStatus"  /commit:apphost
-# & $AppCmd set config  -section:system.applicationHost/sites /siteDefaults.logFile.directory:"d:\logs"  /commit:apphost
+# & $AppCmd set config  -section:system.applicationHost/sites /siteDefaults.logFile.directory:"d:\logs"  /commit:apphost # Configuração post deployment
