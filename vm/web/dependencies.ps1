67df64771608541d5a83fa4c7c094ab073c2ea80
vinicius.sfernandes <vinicius.sfernandes@linx.com.br>
2023-04-04 14:22:12 -0300

    update dependencies vm web

--- vm/web/dependencies.ps1
+++ vm/web/dependencies.ps1
@@ -5,7 +5,7 @@ Install-PackageProvider Nuget -ForceBootstrap -Force -Confirm:$false
 choco feature disable -n=showDownloadProgress; choco feature enable -n=allowGlobalConfirmation
 
 # Install Dotnet with Choco
-choco install dotnet-2.2.8-runtime -y --force --force-dependencies --ignore-checksums
+choco install dotnetcore-runtime --version=2.2.8 -y --force --force-dependencies --ignore-checksums
 choco install dotnetcore-3.1-windowshosting -y --force --force-dependencies --ignore-checksums
 choco install dotnet-5.0-runtime -y --force --force-dependencies --ignore-checksums
 choco install dotnet-5.0-windowshosting -y --force --force-dependencies --ignore-checksums
